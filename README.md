f93f32985fdf165f78b2d1bff48e37e89a752212
Arham Qureshi <arham.qureshi@hotmail.com>
2020-12-06 21:59:34 +1100

    Added files

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# ALVC
-Apex Legends Video Clipper
+# Apex Legends Video Clipper
+Automatically create clips of your knocks/elimations in your Apex Legends Gameplay!
+
+## Purpose
+It can get very tedious and time consuming scrolling through your gameplay to find knocks and eliminations. The purpose of this application is to save you time by automatically creating small clips of your knocks/eliminations.
+
+## How It Works
+The program analyses the frames in your gameplay recordings and looks for the big red hitmarker that appears when an enemy is knocked down or eliminated. The timestamp of each knock/elimination is recorded and then the video is split into clips of each knock/elimination found. 
+
+**IMPORTANT**: You must enable hitmarkers in your gameplay settings or else the program will not detect any knocks/elims. At the moment the detection is only based on hitmarkers however the program may be improved to use other means of detection in the future.
+
+## Program Settings
+The following settings can be set in the program:
+
+| Setting | Description |
+|----------------|-------------|
+|**Samples**|This value determines the amount of frames in the video that will be analysed for hitmarkers. The default value is 30, which means that every 30th frame in the video will be analysed. Lower values provide more accuracy, but increase the processing time. Higher values will lower the processing time, but will provide worse accuracy. The optimal value is half of your gameplay recording FPS. For example, if you record your gameplay at 60FPS you should set this value to 30.|
+|**Seconds to Capture**|These two values determine how many seconds should be captured before and after the knock/elimination in the clip.|
+|**Delete Original File**|If set to yes, the original gameplay video will be permanently deleted. You should keep this set to "No" unless you're absolutely sure you won't need the original video.|
+|**Input & Output Folders**|The first directory should be where your gameplay recordings are stored and the second directory is where you want the clips to be saved. The program will analyse every compatible video in the input directory.|
+
+#### Note
+A file called ".alvc-settings.json" is created in your home directory (C:/Users/Your Profile) when launching the program for the first time. The purpose of this file is to store your settings so they don't change to the default values every time you launch the program.
+
+### Cancelling The Process
+Cancelling the process will only prevent the current and future videos from being processed, but all previous actions will not be reversed. The program makes changes as the process runs, so if the previous videos had been marked for deletion, they will be deleted and the program cannot reverse this.
+
+### Supported File Formats
+- MP4
+
+## Issues
+If you have encountered an issue with the program please create a [Bug Report](https://github.com/arhamqureshi/ALVC/issues/new/choose) in the Issues section of the GitHub Repository. Please provide as much information as you can including screenshots if possible!
+
+## Support Me!
+[![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=WALNKNVGR4L8S&currency_code=AUD)
+
---
+++ analyse_image.py
@@ -0,0 +1,104 @@
+# from skimage.measure import compare_ssim as ssim
+from skimage.metrics import structural_similarity as ssim
+import numpy as np
+import argparse, cv2, sys, os
+
+def resource_path(relative_path):
+	"""
+	Taken from https://stackoverflow.com/a/49802075
+	Resolves an issue with .exe made using PyInstaller
+	"""
+	if hasattr(sys, '_MEIPASS'):
+		return os.path.join(sys._MEIPASS, relative_path)
+	return os.path.join(os.path.abspath("."), relative_path)
+
+def manipulate(lower, upper, image):
+	"""
+	Crops, and manipulates the color of the image to identify the hitmarker. The image
+	is converted to grayscale at the end to make the comparison easier.
+
+	:param lower: A list of ints which define the lower bound of the color (in BGR)
+	:param upper: A list of ints which define the upper bound of the color (in BGR)
+	:param image: The image to be processed (cv2 object)
+	:returns output: The processed image (cv2 object)
+	"""
+	# create NumPy arrays from the boundaries
+	lower = np.array(lower, dtype = "uint8")
+	upper = np.array(upper, dtype = "uint8")
+	# find the colors within the specified boundaries and apply
+	
+	mask = cv2.inRange(image, lower, upper) # the mask
+	output = cv2.bitwise_and(image, image, mask=mask)
+
+	# Remove the content in the middle of the image, otherwise it will impact the
+	# MSE score in an unwanted manner. Hitmarker may be there but the content in the 
+	# middle of the image of the same colour could give an inaccurate score
+	triangle = np.array([[50, 50 + 50], [50, 50 + 50], [50, 50 - 50], [50 + 50, 50]])
+	color = [0, 0, 0]
+	cv2.fillConvexPoly(output, triangle, color)
+	triangle = np.array([[50, 50 + 50], [50, 50 + 50], [50, 50 - 50], [50 - 50, 50]])
+	color = [0, 0, 0]
+	cv2.fillConvexPoly(output, triangle, color)
+	
+	output = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+
+	return output
+
+def compare(expected, img):
+	"""
+	Compares the expected hitmarker image with the passed image to check whether
+	the hitmarker is in the passed image. 
+
+	:param expected: A cv2 object of the image that has the expected hitmarker
+	:param img: A cv2 object of the image we want to check for the hitmarker
+	:returns Bool, mse_err: Returns whether img has the hitmarker or not, and the MSE score
+	"""
+	x = int((img.shape[1] - 100) / 2) 
+	y = int((img.shape[0] - 100) / 2) 
+
+	h = 100
+	w = 100
+
+	image = img[y:y + h, x:x + w] # Crop
+
+	boundaries = [
+		([15, 42, 191], [89, 109, 255]),
+		([15, 42, 215], [89, 109, 255]),
+		([56, 21, 209], [222, 209, 255])
+	]
+
+	mse_threshold = 220
+	mse_err = 0
+	for (lower, upper) in boundaries:
+		output = manipulate(lower, upper, image)
+		mse_err = mse(expected, output)
+		if mse_err < mse_threshold:
+			return True, mse_err
+	return False, mse_err
+
+def mse(image_a, image_b):
+	"""
+	Calculate the Mean Squared Error between the two images
+
+	:param image_a: The first image
+	:param image_b: The second image
+	:returns err: A float
+	"""
+	err = np.sum((image_a.astype("float") - image_b.astype("float")) ** 2)
+	err /= float(image_a.shape[0] * image_a.shape[1])
+	return err
+
+def get_expected_result():
+	"""
+	The expected image of the hitmarker needs to go through the same manipulation
+	as the other images for an accurate comparison. I believe it has something to do
+	with how the image is structured in the np.array after the manipulatio
+	"""
+	return manipulate([15, 42, 191], [89, 109, 255], cv2.imread(resource_path("expected.png")))
+
+if __name__ == "__main__":
+	# For Debugging
+	img1 = get_expected_result()
+	result, score = compare(img1, cv2.imread("hitmarker5.png"))
+	print(result, score)
+
